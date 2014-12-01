052e1a59d2aa8074eb4787c8a11be13cfc4627e6
giuliarusso <g.russo1994@gmail.com>
2014-12-01 10:53:24 +0100

    Ciao, iniziamo il lavoro

---
+++ Gestione Cartella Sanitaria/src/Paziente.java
@@ -0,0 +1,20 @@
+
+public class Paziente
+{
+	String nome;
+	String cognome;
+	String indirizzo;
+	String numTelefono;
+	String email;
+	String dataNascita;
+	String luogoNascita;
+	char sesso;
+	String codiceFiscale;
+	int codiceSanitario;
+	String gruppoSanguigno;
+
+	
+	
+	
+
+}
